2087c4a6b76d83ce64adcaf2e71b637e0af16c3c
Xijian Han <jhinmcglynn@gmail.com>
2021-08-10 13:20:15 -0400

    Updates

---
+++ precache-manifest.52da97ab60f8b13db21db848f99559c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449a2f23a6da2752528d6d7072a4b0e7",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "560376e1fe220653983d",
+    "url": "/test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22b4f6f9ba527f6fe721",
+    "url": "/test/static/css/main.5fc75b9c.chunk.css"
+  },
+  {
+    "revision": "560376e1fe220653983d",
+    "url": "/test/static/js/2.f9cb3fc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test/static/js/2.f9cb3fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b4f6f9ba527f6fe721",
+    "url": "/test/static/js/main.f992bcf8.chunk.js"
+  },
+  {
+    "revision": "2a7bc736a57cacf64f6d",
+    "url": "/test/static/js/runtime-main.1bf31893.js"
+  },
+  {
+    "revision": "e4835645c4abb71b1f6337b72d423ce7",
+    "url": "/test/static/media/bg.e4835645.jpg"
+  },
+  {
+    "revision": "72eb1d045ba989b0f592a46599361147",
+    "url": "/test/static/media/blueUniverse.72eb1d04.jpg"
+  }
+]);
